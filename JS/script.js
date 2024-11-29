c95ea09aa0bab51f2d2b3877355388eeff0b0a41
LiviaTrindade <127799847+LiviaTrindade@users.noreply.github.com>
2024-11-28 23:48:09 -0300

    left tree script

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
 let text = document.getElementById('text');
+let treeLeft = document.getElementById('tree-left');
+let treeRight = document.getElementById('tree-right');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
+    treeLeft.style.left = value * -1.5 + 'px';
 })
